cd9854562e5d24a32945c8fa327f301c7738a36d
mohammed-ben-cheikh <bencheikh.official@gmail.com>
2025-12-15 10:33:08 +0100

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,7 @@
+import "dotenv";
+import app from "./app.js";
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`the server is running in http://localhost:${PORT}`);
+});
